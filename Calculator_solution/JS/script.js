8a87902b25ee9df62d65af82696bb8d040105670
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2022-05-23 07:03:35 +0300

    Add theme change

--- Calculator_solution/JS/script.js
+++ Calculator_solution/JS/script.js
@@ -1,7 +1,19 @@
+//function to delete only one character
 function del(){
     let text_input=document.querySelector('#console').value;
     //delete one character
     let updated_text=text_input.substr(0,text_input.length-1);
     document.querySelector('#console').value=updated_text;
 }
-let theme_value=document.querySelector();
+const styleSheetLinks=document.querySelectorAll("link");
+console.log(styleSheetLinks);
+//changing theme based on the value of the theme
+function changeTheme(themeValue){
+    if(themeValue==1){
+        styleSheetLinks[2].setAttribute('href',"");
+    }
+    else{
+        console.log("Reached here");
+        styleSheetLinks[2].setAttribute('href',`Styles/theme_${themeValue}.css`);
+    }
+}
