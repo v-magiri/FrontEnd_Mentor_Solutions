f2b35da6724ba0466b5deea6fbbeac0b19654ede
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2022-06-03 21:39:34 +0300

    Cart Functionality Working

--- Ecommerce_product_page_Solution/JS/script.js
+++ Ecommerce_product_page_Solution/JS/script.js
@@ -1,7 +1,19 @@
 const hambugerMenu=document.querySelector('.hambuger_menu');
 const menu=document.querySelector('.menu');
 const closeMenu=document.querySelector('.closeMenu');
+const cartItemList=document.querySelector('.my-cart');
+const cartIcon=document.querySelector('.cart');
+const addItem=document.querySelector(".increment");
+const lessItem=document.querySelector('.decrement');
+const quantity=document.querySelector('.quantity');
+const heroImage=document.querySelector('.main_product_img');
+const thumbNailImage=document.querySelectorAll('.thumbnailImg');
+let cartItemCount=quantity.innerHTML;
+//to show and hide the cart details page 
 closeMenu.addEventListener('click',HideMenu);
+cartIcon.addEventListener('click',() => {
+    cartItemList.classList.toggle('hidden');
+});
 hambugerMenu.addEventListener('click',ShowMenu);
 function HideMenu(){
     menu.classList.add("hidden");
@@ -9,3 +21,40 @@ function HideMenu(){
 function ShowMenu(){
     menu.classList.remove('hidden');
 }
+
+//decrementing Items
+lessItem.addEventListener('click',() =>{
+    // console.log(quantity.innerHTML);
+
+    if(cartItemCount>0){
+        cartItemCount--;
+        if(cartItemCount>=0) {
+            quantity.innerHTML=cartItemCount;
+        }
+        
+    }
+});
+//incrementing the product counter 
+addItem.addEventListener('click',() =>{
+    if(cartItemCount>=0){
+        cartItemCount++;
+        if(cartItemCount>0){
+            quantity.innerHTML=cartItemCount;
+        }
+    }
+});
+
+// handle clicks once an thumb Nail Image is clicked
+//remove the current Active Class
+thumbNailImage.forEach(img =>{
+    img.addEventListener('click',onThumbNailClick)
+});
+function onThumbNailClick(e){
+    thumbNailImage.forEach(image=>{
+        image.classList.remove('active')    ;
+    });
+    e.target.classList.add('active');
+    //change to the corresponding Hero Image
+    heroImage.src
+}
+console.log(thumbNailImage);
