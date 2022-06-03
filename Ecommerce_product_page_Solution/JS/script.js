b2e5d1d64c4bf6ad778938eb6867a8834c9240c7
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2022-06-03 05:46:02 +0300

    Add Interactivity

--- Ecommerce_product_page_Solution/JS/script.js
+++ Ecommerce_product_page_Solution/JS/script.js
@@ -1,3 +1,11 @@
-function showMenu(){
-    
+const hambugerMenu=document.querySelector('.hambuger_menu');
+const menu=document.querySelector('.menu');
+const closeMenu=document.querySelector('.closeMenu');
+closeMenu.addEventListener('click',HideMenu);
+hambugerMenu.addEventListener('click',ShowMenu);
+function HideMenu(){
+    menu.classList.add("hidden");
+}
+function ShowMenu(){
+    menu.classList.remove('hidden');
 }
